774f659b98ac2f52e0ccd7be319b314a1c3f6221
Phobetore <broucewayn1413@protonmail.com>
2023-04-14 22:15:30 +0200

    accueil listing traversee using the crud

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Traversee;
+use App\Repository\TraverseeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/accueil', name: 'app_accueil')]
-    public function index(): Response
+    public function index(TraverseeRepository $traverseeRepository): Response
     {
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'traversees' => $traverseeRepository->findAll(),
         ]);
     }
 }
